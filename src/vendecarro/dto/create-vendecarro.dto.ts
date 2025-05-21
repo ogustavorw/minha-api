16a4857d433517bd27f74417a514c72fdd668754
ogustavorw <gustavowoicekoscki@hotmail.com>
2025-05-21 19:24:23 -0300

    ajustado import do class validator

--- src/vendecarro/dto/create-vendecarro.dto.ts
+++ src/vendecarro/dto/create-vendecarro.dto.ts
@@ -1,6 +1,12 @@
+import {
+    IsInt,
+    IsString
+} from 'class-validator'
+
+
 export class CreateVendecarroDto {
     @IsInt()
-    id: number; // Id do carro
+    // Removi a chamada do ID, pois ao cadastrar um novo carro, estava dando erro. Cadastrei um carro e no banco de dados, identifiquei que o mesmo foi inserido com o id = 0, ao tentar cadastrar um novo carro, o processo não foi concluido.
     manufactureYear: number; // Ano de fabricação do carro
     horsepower: number; // potência do motor em cavalos (hp)
     @IsString() 
@@ -8,9 +14,3 @@ export class CreateVendecarroDto {
     model: string; // Nome/Modelo do carro
     engine: string; // Motor 4.0 V8
 }
-
-import {
-    IsInt,
-    IsString
-} from 'class-validator'
-
